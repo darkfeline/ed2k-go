fdfa0993a0a81189a2c35ea50befd976ba680b25
Allen Li <darkfeline@felesatra.moe>
2026-01-05 16:09:14 -0800

    Deduplicate tests

--- ed2k_test.go
+++ ed2k_test.go
@@ -21,133 +21,73 @@ import (
 	"go.felesatra.moe/hash/ed2k"
 )
 
+// testBytes deduplicates bytes for testing.
+var testBytes [2 * 9728000]byte
+
 func TestHash(t *testing.T) {
 	t.Parallel()
-	t.Run("new", func(t *testing.T) {
-		t.Parallel()
-		h := ed2k.New()
-		sum := h.Sum(nil)
-		want := "31d6cfe0d16ae931b73c59d7e0c089c0"
-		got := fmt.Sprintf("%x", sum)
-		if got != want {
-			t.Errorf("Got %s, want %s", got, want)
-		}
-	})
-	t.Run("empty write", func(t *testing.T) {
-		t.Parallel()
-		data := make([]byte, 0)
-		h := ed2k.New()
-		_, err := h.Write(data)
-		if err != nil {
-			t.Fatal(err)
-		}
-		sum := h.Sum(nil)
-		want := "31d6cfe0d16ae931b73c59d7e0c089c0"
-		got := fmt.Sprintf("%x", sum)
-		if got != want {
-			t.Errorf("Got %s, want %s", got, want)
-		}
-	})
-	t.Run("one block", func(t *testing.T) {
-		t.Parallel()
-		data := make([]byte, 9728000)
-		h := ed2k.New()
-		_, err := h.Write(data)
-		if err != nil {
-			t.Fatal(err)
-		}
-		sum := h.Sum(nil)
-		want := "d7def262a127cd79096a108e7a9fc138"
-		got := fmt.Sprintf("%x", sum)
-		if got != want {
-			t.Errorf("Got %s, want %s", got, want)
-		}
-	})
-	t.Run("two blocks", func(t *testing.T) {
-		t.Parallel()
-		data := make([]byte, 19456000)
-		h := ed2k.New()
-		_, err := h.Write(data)
-		if err != nil {
-			t.Fatal(err)
-		}
-		sum := h.Sum(nil)
-		want := "194ee9e4fa79b2ee9f8829284c466051"
-		got := fmt.Sprintf("%x", sum)
-		if got != want {
-			t.Errorf("Got %s, want %s", got, want)
-		}
-	})
-	t.Run("one block multi writes", func(t *testing.T) {
-		t.Parallel()
-		data := make([]byte, 9728000)
-		h := ed2k.New()
-		_, err := h.Write(data[:1000000])
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, err = h.Write(data[1000000:])
-		if err != nil {
-			t.Fatal(err)
-		}
-		sum := h.Sum(nil)
-		want := "d7def262a127cd79096a108e7a9fc138"
-		got := fmt.Sprintf("%x", sum)
-		if got != want {
-			t.Errorf("Got %s, want %s", got, want)
-		}
-	})
-	t.Run("two block blockwise writes", func(t *testing.T) {
-		t.Parallel()
-		data := make([]byte, 19456000)
-		h := ed2k.New()
-		_, err := h.Write(data[:9728000])
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, err = h.Write(data[9728000:])
-		if err != nil {
-			t.Fatal(err)
-		}
-		sum := h.Sum(nil)
-		want := "194ee9e4fa79b2ee9f8829284c466051"
-		got := fmt.Sprintf("%x", sum)
-		if got != want {
-			t.Errorf("Got %s, want %s", got, want)
-		}
-	})
-	t.Run("two block multi writes", func(t *testing.T) {
-		t.Parallel()
-		data := make([]byte, 19456000)
-		h := ed2k.New()
-		_, err := h.Write(data[:10728000])
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, err = h.Write(data[10728000:])
-		if err != nil {
-			t.Fatal(err)
-		}
-		sum := h.Sum(nil)
-		want := "194ee9e4fa79b2ee9f8829284c466051"
-		got := fmt.Sprintf("%x", sum)
-		if got != want {
-			t.Errorf("Got %s, want %s", got, want)
-		}
-	})
-	t.Run("one and partial blocks", func(t *testing.T) {
-		t.Parallel()
-		data := make([]byte, 9728001)
-		h := ed2k.New()
-		_, err := h.Write(data[:9728001])
-		if err != nil {
-			t.Fatal(err)
-		}
-		sum := h.Sum(nil)
-		want := "06329e9dba1373512c06386fe29e3c65"
-		got := fmt.Sprintf("%x", sum)
-		if got != want {
-			t.Errorf("Got %s, want %s", got, want)
-		}
-	})
+	cases := []struct {
+		name string
+		data [][]byte
+		want string
+	}{
+		{
+			name: "new",
+			data: nil,
+			want: "31d6cfe0d16ae931b73c59d7e0c089c0",
+		},
+		{
+			name: "empty write",
+			data: [][]byte{testBytes[:0]},
+			want: "31d6cfe0d16ae931b73c59d7e0c089c0",
+		},
+		{
+			name: "one block",
+			data: [][]byte{testBytes[:9728000]},
+			want: "d7def262a127cd79096a108e7a9fc138",
+		},
+		{
+			name: "one block multi writes",
+			data: [][]byte{testBytes[:1000000], testBytes[:9728000-1000000]},
+			want: "d7def262a127cd79096a108e7a9fc138",
+		},
+		{
+			name: "one and partial blocks",
+			data: [][]byte{testBytes[:9728000+1]},
+			want: "06329e9dba1373512c06386fe29e3c65",
+		},
+		{
+			name: "two block",
+			data: [][]byte{testBytes[:2*9728000]},
+			want: "194ee9e4fa79b2ee9f8829284c466051",
+		},
+		{
+			name: "two block blockwise writes",
+			data: [][]byte{testBytes[:9728000], testBytes[:9728000]},
+			want: "194ee9e4fa79b2ee9f8829284c466051",
+		},
+		{
+			name: "two block multi writes",
+			data: [][]byte{testBytes[:10728000], testBytes[:2*9728000-10728000]},
+			want: "194ee9e4fa79b2ee9f8829284c466051",
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			h := ed2k.New()
+			for _, d := range c.data {
+				_, err := h.Write(d)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+			sum := h.Sum(nil)
+			got := fmt.Sprintf("%x", sum)
+			if got != c.want {
+				t.Errorf("Got %s, want %s", got, c.want)
+			}
+		})
+
+	}
 }
